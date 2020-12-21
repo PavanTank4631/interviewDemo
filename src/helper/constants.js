e3b22473e499912851bbc4ad8d0fba436a0c9a5a
PavanTank4631 <pavantank7016@gmail.com>
2020-12-21 15:16:03 +0530

    secured zomato key

--- src/helper/constants.js
+++ src/helper/constants.js
@@ -1,2 +1,4 @@
 export const BASE_URL = 'https://developers.zomato.com/api/v2.1/';
-export const ZOMATOKEY = 'd53217c7f8cd077250b08bc76eb3d983';
+export const ZOMATOKEY = '';
+//to fetch restaurants in your city you will need zomato api key from your zomato developer account, you can do by simply go with below link
+//https://developers.zomato.com/api
